2255a74fbaf246ea3dbca5ac5ac6bb0de4228325
Manvitha Anandini <manvithasure06@gmail.com>
2026-07-04 22:25:23 +0530

    Initial commit

---
+++ Character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Character{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char c1 = sc.next().charAt(0);
+        char c2 = sc.next().charAt(0);
+        if(c1 < c2)
+            System.out.println(c1 +" "+c2);
+        else
+            System.out.println(c2+" "+c1);
+    }
+}
