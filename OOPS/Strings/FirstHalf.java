d162e555a01585498a86bec79e27d67f4ecc97aa
Manvitha Anandini <manvithasure06@gmail.com>
2026-07-09 23:22:48 +0530

    Return first half

---
+++ OOPS/Strings/FirstHalf.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class FirstHalf{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        if(s.length() % 2 == 0){
+            System.out.println(s.substring(0, s.length()/2));
+        }
+        else
+            System.out.println("Null");
+    }
+}
