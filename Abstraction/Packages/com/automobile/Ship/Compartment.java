d1b358bc1baa9b02fd86d18eb251ab2b12bd5f1b
Manvitha Anandini <manvithasure06@gmail.com>
2026-07-11 08:21:34 +0530

    Commit

---
+++ Abstraction/Packages/com/automobile/Ship/Compartment.java
@@ -0,0 +1,20 @@
+package com.automobile.ship;
+public class Compartment{
+    private int height;
+    private int width;
+    private int breadth;
+    public Compartment(int height, int width, int breadth){
+        this.height = height;
+        this.width = width;
+        this.breadth = breadth;
+    }
+    public void display(){
+        System.out.println("Height: "+height);
+        System.out.println("Width: "+width);
+        System.out.println("Breadth: "+breadth);
+    }
+    public static void main(String[] args){
+        Compartment c = new Compartment(20, 15, 10);
+        c.display();
+    }
+}
