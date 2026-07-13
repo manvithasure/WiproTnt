6461413f762110e25c84be0b746397a38956bd0e
Manvitha Anandini <manvithasure06@gmail.com>
2026-07-13 17:18:16 +0530

    Added months in array list

---
+++ Collections/Lists/Months.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Months{
+    public static void main(String[] args){
+        ArrayList<String> months = new ArrayList<>();
+        months.add("January");
+        months.add("February");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("August");
+        months.add("September");
+        months.add("October");
+        months.add("November");
+        months.add("December");
+        System.out.print(months+" ");
+    }
+}
