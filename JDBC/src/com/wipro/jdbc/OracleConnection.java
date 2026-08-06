8c6877ded08798306e7def995dcdc9a8f2fde2f6
Manvitha Anandini <manvithasure06@gmail.com>
2026-08-06 11:11:05 +0530

    Established JDBC Connection

---
+++ JDBC/src/com/wipro/jdbc/OracleConnection.java
@@ -0,0 +1,28 @@
+package com.wipro.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class OracleConnection {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+        String username = "hr";
+        String password = "hr";   // Replace with your HR password
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            System.out.println("Connection Established successfully");
+
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println("Connection could not be established");
+            System.out.println(e);
+        }
+    }
+}
